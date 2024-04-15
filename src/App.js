9f28cfe64b090c75ce24f88b0bf1c2ea60aafcd3
Many Richard Porras Avila <44829219+Many-Porras@users.noreply.github.com>
2024-04-15 14:05:10 -0500

    Add files via upload
    demo-rimac

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Router, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="">
+      <Router>
+        <Routes>
+        
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
